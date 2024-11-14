12b14e90882009e56ccae17f1d04c00e2f62662f
Jordan Walker <ludalupus@gmail.com>
2024-11-14 10:53:56 -0800

    attempting to add hash routing in order to enable nested link access

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase-config.js";
 import TagItems from "./pages/TagItems.js";
@@ -10,51 +10,51 @@ import Closet from "./pages/Closet.js";
 import FavFits from './pages/FavFits.js';
 import Search from './pages/Search.js';
 import Profile from './pages/Profile.js';
-import AutoTag from './pages/AutoTag.js'; // Import the AutoTag page
+import AutoTag from './pages/AutoTag.js';
 import Feed from './pages/Feed.js';
 
 function App() {
-  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+    const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
 
-  const signUserOut = () => {
-    signOut(auth).then(() => {
-      localStorage.clear();
-      setIsAuth(false);
-      window.location.pathname = "/onlineclosetbeta/login";
-    });
-  }
+    const signUserOut = () => {
+        signOut(auth).then(() => {
+            localStorage.clear();
+            setIsAuth(false);
+            window.location.pathname = "/onlineclosetbeta/login";
+        });
+    }
 
-  return (
-    <Router basename="/onlineclosetbeta">
-          <nav>
-              {isAuth && <Link to="/feed">Feed</Link>}
-        {isAuth && <Link to="/" onClick={() => {localStorage.removeItem("closetID")}}>Closet</Link>}
-        {isAuth && <Link to="/fits">Outfits</Link>}
-        {isAuth && <Link to="/tagitems">Tag Items</Link>}
-        {isAuth && <Link to="/profile">Profile</Link>}
-        <Link to="/search">Search</Link>
-        {!isAuth ? <Link to="/login">Login</Link> : 
-        <>
-          <button className="signOutBtn" onClick={signUserOut}>Log Out</button>
-        </>
-        }
-          </nav>
-      <div className="main-content">
-      <Routes>
-        <Route path="/tagitems" element={<TagItems isAuth={isAuth} />} />
-        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
-        <Route path="/swipe" element={<SwipeFits />} />
-        <Route path="/fits" element={<FavFits isAuth={isAuth} />} />
-        <Route path="/search" element={<Search isAuth={isAuth} />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/:profileId" element={<Profile />} />
-        <Route path="/" element={<Closet isAuth={isAuth} />} />
-        <Route path="/:profileId" element={<Closet isAuth={isAuth} />} />
-        <Route path="/feed" element={<Feed isAuth={isAuth}/> } />
-        </Routes>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <nav>
+                {isAuth && <Link to="/feed">Feed</Link>}
+                {isAuth && <Link to="/" onClick={() => { localStorage.removeItem("closetID") }}>Closet</Link>}
+                {isAuth && <Link to="/fits">Outfits</Link>}
+                {isAuth && <Link to="/tagitems">Tag Items</Link>}
+                {isAuth && <Link to="/profile">Profile</Link>}
+                <Link to="/search">Search</Link>
+                {!isAuth ? <Link to="/login">Login</Link> :
+                    <>
+                        <button className="signOutBtn" onClick={signUserOut}>Log Out</button>
+                    </>
+                }
+            </nav>
+            <div className="main-content">
+                <Routes>
+                    <Route path="/tagitems" element={<TagItems isAuth={isAuth} />} />
+                    <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+                    <Route path="/swipe" element={<SwipeFits />} />
+                    <Route path="/fits" element={<FavFits isAuth={isAuth} />} />
+                    <Route path="/search" element={<Search isAuth={isAuth} />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/profile/:profileId" element={<Profile />} />
+                    <Route path="/" element={<Closet isAuth={isAuth} />} />
+                    <Route path="/:profileId" element={<Closet isAuth={isAuth} />} />
+                    <Route path="/feed" element={<Feed isAuth={isAuth} />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
